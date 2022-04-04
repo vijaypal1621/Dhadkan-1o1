9091b65708141c89de2937f28694768357fa0c6a
Aaryan Raj Sarda <aaryanrajsarda@gmail.com>
2022-04-04 20:30:52 +0530

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Home from './Home';
+import "./App.css";
+import Home from "./Home";
 import Header from "./Header";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from "react";
 // import firebase from '../src/firebase.js';
 // import firebase from 'firebase';
 // import db from './firebase';
@@ -11,15 +11,13 @@ import React, {useEffect,useState} from 'react';
 // import {db} from './firebase';
 // import {onValue, ref} from 'firebase/database';
 
-
 function App() {
-
   // const [pulseRate , setPulseRate] = useState("");
-  
+
   // useEffect(()=>{
   //   onValue(ref(db),(snapshot) =>{
   //     setPulseRate(snapshot.val().pulse);
-      
+
   //     // console.log(pulseRate);
   //     console.log(snapshot.val().pulse);
   //     console.log(pulseRate);
@@ -27,24 +25,21 @@ function App() {
 
   // }, []);
 
-
-
   return (
-    // 
     <BrowserRouter>
-        <Header />
-        <Switch>
-        <Route exact path='/' component={Home}/>
-        </Switch>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </BrowserRouter>
     // <BrowserRouter>
-    //     <div className="app">         
-              
+    //     <div className="app">
+
     //           {/* <h1> kjjfjd</h1>
     //           <div className="app__body ">
     //             <Switch>
     //                <Route path="/home" component={Home} />
-    //                <Route path="/societies" component={Society} /> 
+    //                <Route path="/societies" component={Society} />
     //                <Route path="/resources" component={Resources} />
     //               <Redirect to="/home" />
     //             </Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,119 +1,93 @@
 import React from "react";
 import "./Home.css";
-import Zone from './Zone.js';
-import level0 from './level0.png';
-import level2 from './level2.png';
-import level3 from './level3.png';
-import level4 from './level4.png';
-import level5 from './level5.png';
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from "react";
 // import firebase from '../src/firebase.js';
 // import firebase from 'firebase';
 // import db from './firebase';
 // import firebase from './firebase';
 // import firebase from 'firebase';
-import {db} from './firebase';
-import {onValue, ref} from 'firebase/database';
-import CircularSlider from '@fseehawer/react-circular-slider';
-import Thermometer from 'react-thermometer-component'
-import GaugeChart from 'react-gauge-chart'
+import { db } from "./firebase";
+import { onValue, ref } from "firebase/database";
+import CircularSlider from "@fseehawer/react-circular-slider";
 
+function Home() {
+  const [pulseRate, setPulseRate] = useState("");
 
-  
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      setPulseRate(snapshot.val().pulse);
 
-// function rangeColor( pulseCount)
-// {
-//     if(pulseCount >= 160){
-//         return "#ff2052";
-//     }
-//     else if(pulseCount >= 120){
-//         return "#ed872d";
-//     }
-//     else if(pulseCount >= 90){
-//         return "#03c03c";
-//     }
-//     else if(pulseCount >= 60){
-//         return "#007fff";
-//     }else if(pulseCount >= 50){
-//         return "#a2a2d0";
-//     }
-//     else{
-//         return "grey";
-//     }
-// }
+      // console.log(pulseRate);
+      // console.log(snapshot.val().pulse);
+      // console.log(pulseRate);
+    });
+  }, [pulseRate]);
 
-
-
-function Home(){
-
-    const [pulseRate , setPulseRate] = useState("");
-      
-    useEffect(()=>{
-      onValue(ref(db),(snapshot) =>{
-        setPulseRate(snapshot.val().pulse);
-        
-        // console.log(pulseRate);
-        // console.log(snapshot.val().pulse);
-        // console.log(pulseRate);
-      } );
-  
-    }, [pulseRate]);
-
-
-    return (
-        <div className="container">
-        <div className="row">
-            <div className="col-4">
-            <CircularSlider label="Temperature"
-                    labelColor="#005a58"
-                    knobColor="#005a58"
-                    progressColorFrom="#00bfbd"
-                    progressColorTo="#009c9a"
-                    progressSize={24}
-                    trackColor="#eeeeee"
-                    trackSize={24}
-                    max={212}
-                    min = {32}
-                    dataIndex = {97}
-                    appendToValue={"F"}
-                    onChange={ value => { console.log(value); } }/>
-            </div>
-            <div className="col-4">
-            <div>
-                <CircularSlider label="Pulse"
-                    labelColor="#005a58"
-                    knobColor="#005a58"
-                    progressColorFrom="#00bfbd"
-                    progressColorTo="#009c9a"
-                    progressSize={24}
-                    trackColor="#eeeeee"
-                    trackSize={24}
-                    max={200}
-                    dataIndex = {pulseRate}
-                    appendToValue={"bpm"}
-                    onChange={ value => { console.log(value); } }/>
-                </div>
-            </div>
-            <div className="col-4">
-            <div>
-            <CircularSlider label="SpO2"
-                    labelColor="#005a58"
-                    knobColor="#005a58"
-                    progressColorFrom="#00bfbd"
-                    progressColorTo="#009c9a"
-                    progressSize={24}
-                    trackColor="#eeeeee"
-                    trackSize={24}
-                    max={100}
-                    dataIndex = {80}
-                    appendToValue={"%"}
-                    onChange={ value => { console.log(value); } }/>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <CircularSlider
+            label="Temperature"
+            labelColor="#005a58"
+            knobColor="#005a58"
+            progressColorFrom="#00bfbd"
+            progressColorTo="#009c9a"
+            progressSize={24}
+            trackColor="#eeeeee"
+            trackSize={24}
+            max={212}
+            min={32}
+            dataIndex={97}
+            appendToValue={"F"}
+            onChange={(value) => {
+              console.log(value);
+            }}
+          />
         </div>
+        <div className="col-4">
+          <div>
+            <CircularSlider
+              label="Pulse"
+              labelColor="#005a58"
+              knobColor="#005a58"
+              progressColorFrom="#00bfbd"
+              progressColorTo="#009c9a"
+              progressSize={24}
+              trackColor="#eeeeee"
+              trackSize={24}
+              max={200}
+              dataIndex={pulseRate}
+              appendToValue={"bpm"}
+              onChange={(value) => {
+                console.log(value);
+              }}
+            />
+          </div>
         </div>
-    )
-
-};
+        <div className="col-4">
+          <div>
+            <CircularSlider
+              label="SpO2"
+              labelColor="#005a58"
+              knobColor="#005a58"
+              progressColorFrom="#00bfbd"
+              progressColorTo="#009c9a"
+              progressSize={24}
+              trackColor="#eeeeee"
+              trackSize={24}
+              max={100}
+              dataIndex={80}
+              appendToValue={"%"}
+              onChange={(value) => {
+                console.log(value);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
