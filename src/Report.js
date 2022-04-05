29000949dbc5dc52da39ed709d6756711efb6a7d
Vijay Pal <vijay.nsut@gmail.com>
2022-04-05 16:53:09 +0530

    Pdf direction of report added

--- src/Report.js
+++ src/Report.js
@@ -13,7 +13,8 @@ function Report(){
                     <h5>Sat, 10 Apr 2021 18:39:34 GMT</h5>
                 </div>
                 <div className="col-2" style={{"textAlign": "end"}}>
-                    <CloudDownloadIcon />
+                    
+                    <a href="https://firebasestorage.googleapis.com/v0/b/dhadkan-1o1.appspot.com/o/5daf0a22-66e4-486d-a16f-03e020f623e6.pdf?alt=media&token=d37f36bd-03ca-4907-8445-6fdb75fc8df8" target="blank"><CloudDownloadIcon /></a>
                 </div>
             </div>
         
